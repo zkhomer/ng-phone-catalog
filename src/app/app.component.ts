d869502c7aaa9bcb4e414aa8140e4876beb712b7
Artur Bespalyy <zkhomers@gmail.com>
2020-11-18 18:36:04 +0200

    practice ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  posts: Post[] = [
+    {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
+    {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2}
+  ]
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post)
+    // console.log('Post', post)
+  }
 }
